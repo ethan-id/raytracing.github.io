516fc8309c9f2cec96678a32c6fa294efbc6f82c
ehancock <ehancock@iastate.edu>
2025-08-26 23:56:19 -0500

    lambertian

--- src/myWeekend/camera.h
+++ src/myWeekend/camera.h
@@ -91,7 +91,7 @@ class camera {
             hit_record rec;
 
             if (world.hit(r, interval(0.001, infinity), rec)) {
-                vec3 dir = random_on_hemisphere(rec.normal);
+                vec3 dir = rec.normal + random_unit_vector();
                 return 0.5 * ray_color(ray(rec.p, dir), depth-1, world);
             }
 
