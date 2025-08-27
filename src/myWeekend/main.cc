1019bc89df78cab3202d0e1e2c91ef35b0912593
ehancock <ehancock@iastate.edu>
2025-08-26 23:30:30 -0500

    Add max bounces, fix shadow 'acne'

--- src/myWeekend/main.cc
+++ src/myWeekend/main.cc
@@ -10,6 +10,8 @@ int main() {
     hittable_list world;
 
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    /*world.add(make_shared<sphere>(point3(-1,0,-1), 0.3));
+    world.add(make_shared<sphere>(point3(1,0,-1), 0.3));*/
     world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
 
     camera cam;
@@ -17,6 +19,7 @@ int main() {
     cam.aspect_ratio      = 16.0 / 9.0;
     cam.image_w           = 400;
     cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
 
     cam.render(world);
 }
