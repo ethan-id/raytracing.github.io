018385c638f47859bf285862f4fc624d540aff80
ehancock <ehancock@iastate.edu>
2025-08-26 22:32:48 -0500

    Random num gen methods

--- src/myWeekend/rtweekend.h
+++ src/myWeekend/rtweekend.h
@@ -3,6 +3,7 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -21,6 +22,14 @@ const double pi = 3.141592653589792385;
 // Util Funcs
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double ranom_double(double min, double max) {
+    return min + (max-min)*random_double();
 }
 
 // Common Headers
