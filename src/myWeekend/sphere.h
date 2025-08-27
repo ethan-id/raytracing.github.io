1f19d713d5b061a3c984efd847853493cebcc271
ehancock <ehancock@iastate.edu>
2025-08-27 17:39:01 -0500

    Add dielectric material, fix bug setting normal in sphere::hit

--- src/myWeekend/sphere.h
+++ src/myWeekend/sphere.h
@@ -33,7 +33,6 @@ class sphere : public hittable {
             rec.p = r.at(rec.t);
             vec3 outward_normal = (rec.p - center) / radius;
             rec.setFaceNormal(r, outward_normal);
-            rec.normal = (rec.p - center) / radius;
             rec.mat = mat;
 
             return true;
