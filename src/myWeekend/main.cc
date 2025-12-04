5492783ed0bf7ea6e4a682eeff3517a73a1d195a
ehancock <ehancock@iastate.edu>
2025-12-04 14:17:17 -0600

    Update main to test

--- src/myWeekend/main.cc
+++ src/myWeekend/main.cc
@@ -24,7 +24,8 @@ int main() {
                     // diffuse
                     auto albedo = color::random() * color::random();
                     sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    auto center2 = center + vec3(0, randomDouble(0, 0.5), 0);
+                    world.add(make_shared<sphere>(center, center2, 0.2, sphere_material));
                 } else if (choose_mat < 0.95) {
                     // metal
                     auto albedo = color::random(0.5, 1);
@@ -52,8 +53,8 @@ int main() {
     camera cam;
 
     cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_w           = 1200;
-    cam.samples_per_pixel = 500;
+    cam.image_w           = 400;
+    cam.samples_per_pixel = 20;
     cam.max_depth         = 50;
 
     cam.vfov     = 20;
