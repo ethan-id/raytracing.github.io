358252c4337e51e1340191b4e8f302b30f4020f8
ehancock <ehancock@iastate.edu>
2025-12-05 13:42:23 -0600

    Add earth scene

--- src/myWeekend/main.cc
+++ src/myWeekend/main.cc
@@ -98,9 +98,32 @@ void checkeredSpheres() {
     cam.render(world);
 }
 
+void earth() {
+    auto earth_texture = make_shared<imageTexture>("earthmap.jpg");
+    auto earth_surface = make_shared<lambertian>(earth_texture);
+    auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
+
+    camera cam;
+
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_w           = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
+
+    cam.vfov     = 20;
+    cam.lookfrom = point3(0,0,12);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(hittable_list(globe));
+}
+
 int main() {
-    switch (2) {
+    switch (3) {
         case 1: bouncingSpheres();  break;
         case 2: checkeredSpheres(); break;
+        case 3: earth();            break;
     }
 }
