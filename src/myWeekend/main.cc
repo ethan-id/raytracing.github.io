e9b4f8c76ad74edf8d41d98641c66f6d505b574e
ehancock <ehancock@iastate.edu>
2025-12-05 13:19:59 -0600

    Add checkered spheres scene

--- src/myWeekend/main.cc
+++ src/myWeekend/main.cc
@@ -73,6 +73,34 @@ void bouncingSpheres() {
 
 }
 
+void checkeredSpheres() {
+    hittable_list world;
+
+    auto checker = make_shared<checkerTexture>(0.32, color(.2, .3, .1), color(.9, .9, .9));
+
+    world.add(make_shared<sphere>(point3(0,-10, 0), 10, make_shared<lambertian>(checker)));
+    world.add(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
+
+    camera cam;
+
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_w       = 400;
+    cam.samples_per_pixel = 100;
+    cam.max_depth         = 50;
+
+    cam.vfov     = 20;
+    cam.lookfrom = point3(13,2,3);
+    cam.lookat   = point3(0,0,0);
+    cam.vup      = vec3(0,1,0);
+
+    cam.defocus_angle = 0;
+
+    cam.render(world);
+}
+
 int main() {
-    bouncingSpheres();
+    switch (2) {
+        case 1: bouncingSpheres();  break;
+        case 2: checkeredSpheres(); break;
+    }
 }
