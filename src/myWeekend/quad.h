afe7c7a7af954b3f16a6c8be39651867f4ca47f8
ehancock <ehancock@iastate.edu>
2025-12-08 12:13:12 -0600

    Implement quad hit

--- src/myWeekend/quad.h
+++ src/myWeekend/quad.h
@@ -2,12 +2,17 @@
 #define QUAD_H
 
 #include "hittable.h"
+#include "myWeekend/vec3.h"
 
 class quad : public hittable {
   public:
     quad(const point3& Q, const vec3& u, const vec3& v, shared_ptr<material> mat)
       : Q(Q), u(u), v(v), mat(mat)
     {
+        auto n = cross(u, v);
+        normal = convertToUnitVector(n);
+        D = dot(normal, Q);
+        w = n/dot(n,n);
         setBoundingBox();
     }
 
@@ -18,7 +23,21 @@ class quad : public hittable {
     }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-        return false;
+        // If ray is parallel to plane  OR  hit point parameter t is outside the ray interval.
+        //      No hit, return false
+        auto denom = dot(normal, r.direction());
+        auto t = (D - dot(normal, r.origin())) / denom;
+        if (std::fabs(denom) < 1e-8 || !ray_t.contains(t))
+            return false;
+
+        auto intersection = r.at(t);
+
+        rec.t = t;
+        rec.p = intersection;
+        rec.mat = mat;
+        rec.setFaceNormal(r, normal);
+
+        return true;
     }
 
     aabb boundingBox() const override { return bbox; }
@@ -26,8 +45,11 @@ class quad : public hittable {
   private:
     point3 Q;
     vec3 u, v;
+    vec3 w;
     shared_ptr<material> mat;
     aabb bbox;
+    vec3 normal;
+    double D;
 };
 
 #endif
